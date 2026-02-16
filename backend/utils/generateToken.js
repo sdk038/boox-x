82e4346fa3ba7c83618a967ec4c1e6e46f6caa4f
dalersabirov <sobrovv08@gmail.com>
2026-02-16 12:31:33 +0500

    fix: add default JWT_EXPIRE value to prevent token generation error

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
+    expiresIn: process.env.JWT_EXPIRE || '30d'
   });
 };
 
