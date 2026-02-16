10402bd26a58b0299f2caf3bf7968c5030d6ec42
dalersabirov <sobrovv08@gmail.com>
2026-02-16 14:21:50 +0500

    feat: add 'under development' notice to Boards page

--- frontend/src/components/Boards.jsx
+++ frontend/src/components/Boards.jsx
@@ -58,6 +58,11 @@ const Boards = () => {
           </tbody>
         </table>
       </div>
+
+      <div className="dev-notice">
+        <span className="dev-notice-icon">🚧</span>
+        <p>Раздел находится в разработке. Скоро здесь появятся новые возможности!</p>
+      </div>
     </div>
   );
 };
